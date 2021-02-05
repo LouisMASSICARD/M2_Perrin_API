2c8272f96355642aa3c09e23847f4aab797b3d78
MassicardLouis <louis.massicard@orange.fr>
2021-02-05 08:16:53 +0100

    Suppr unesed.

--- old-cours-service/src/main/java/org/miage/m2/boundary/EpisodeResource.java
+++
@@ -1,7 +0,0 @@
-package org.miage.m2.boundary;
-
-import org.miage.m2.entity.Episode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EpisodeResource extends JpaRepository<Episode, String> {
-}
